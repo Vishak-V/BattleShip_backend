314212daebd6477611b7f23c896646dd1c094a34
Chguzenda <chguzenda@crimson.ua.edu>
2025-02-04 14:00:27 -0600

    Updated matchmaker.py
    test code now includes 2 lobbies running simultaneously, with 6 players waiting for matches.

--- matchmaker.py
+++ matchmaker.py
@@ -65,6 +65,8 @@ Lobby.waitingPlayers.push("Player1")
 Lobby.waitingPlayers.push("Player2")
 Lobby.waitingPlayers.push("Player3")
 Lobby.waitingPlayers.push("Player4")
+Lobby.waitingPlayers.push("Player5")
+Lobby.waitingPlayers.push("Player6")
 
 @app.get("/")
 async def root():
@@ -72,12 +74,21 @@ async def root():
 
 @app.on_event("startup")
 @repeat_every(seconds=45, logger=logger)
-async def finishMatch():
-    gameLobby.finishGame() 
-    print("Finished Game\n")
+async def finishMatchForLobby1():
+    gameLobby1.finishGame() 
+    print("Finished Game For Lobby 1\n")
+
+@app.on_event("startup")
+@repeat_every(seconds=38, logger=logger)
+async def finishMatchForLobby2():
+    gameLobby2.finishGame() 
+    print("Finished Game for Lobby 2\n")
 
 @app.on_event("startup")
 @repeat_every(seconds=10, logger=logger)
 async def attemptToMatchmake():
-    output = attemptMatchmaking(gameLobby) 
-    print("Matchmaked", output, "players\n")
+    output1 = attemptMatchmaking(gameLobby1) 
+    output2 = attemptMatchmaking(gameLobby2)
+    print("Matchmaked", output1, "players for Lobby1\n")
+    print("Matchmaked", output2, "players for Lobby2\n")
+
